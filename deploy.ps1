d9bb8672e11d8ae06319e2891d7f4a39e3ea0446
aruvaramakrishna <53849578+aruvaramakrishna@users.noreply.github.com>
2020-09-16 07:22:12 +0530

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,5 +0,0 @@
-#Login-AzAccount
-
-New-AzResourceGroup -Name "ARM_RG" -Location "East US"
-
-New-AzResourceGroupDeployment -ResourceGroupName "ARM_RG" -TemplateFile .\template.json -TemplateParameterFile .\parameters.json -Verbose
