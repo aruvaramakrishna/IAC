469b8299f6a335aaa248aad47185a536131e1f35
aruvaramakrishna <53849578+aruvaramakrishna@users.noreply.github.com>
2020-09-22 08:35:25 +0530

    Add files via upload

---
+++ Terraform/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=2.4.0"
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+
---
+++ Terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "subscription_id" {
+   default     = "bee826dd-cd23-4ca3-971a-2feacbc54b81"
+}
+
+variable "client_id" {
+   default     = "122ea8ba-516d-4344-b58a-37680e467bc7"
+}
+
+variable "client_secret" {
+   default     = "a.PciJ.S58Do~QnEpK6v3OXTX~rrSm1eV8"
+}
+
+variable "tenant_id" {
+   default     = "5fbeac1c-beae-48d4-8f09-540d25732ee1"
+}
+
+variable "resource_group_name" {
+   default     = "terraform_resource_group"
+}
+
+variable "resource_group_location" {
+   default     = "East us"
+}
+variable "virtual_network_name" {
+   default     = "terraform_vnet"
+}
