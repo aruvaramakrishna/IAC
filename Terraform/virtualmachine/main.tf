12796c648acdce0d1b9f3e1a9c14804cec06a219
aruvaramakrishna <53849578+aruvaramakrishna@users.noreply.github.com>
2020-09-23 08:34:22 +0530

    Add files via upload

---
+++ Terraform/virtualmachine/main.tf
@@ -0,0 +1,120 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=2.4.0"
+/*
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+*/
+  features {}
+}
+
+# Terraform backend state
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "chandrurg"
+    storage_account_name = "chandrurgdiag"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a subnet within the resource group
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.0.0/24"
+}
+
+# Create a storage account within the resource group
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = var.storageaccountname
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+# Create a public ip address within the resource group
+resource "azurerm_public_ip" "publicip" {
+  name                = var.publicipaddress
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+# Create a NSG within the resource group
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = 3389
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create a NIC within the resource group
+resource "azurerm_network_interface" "nic" {
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create a Windows Virtual Machine within the resource group
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.windows_virtual_machine_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
---
+++ Terraform/virtualmachine/variables.tf
@@ -0,0 +1,50 @@
+variable "subscription_id" {
+   default     = "bee826dd-cd23-4ca3-971a-2feacbc54b81"
+}
+
+variable "client_id" {
+   default     = ""
+}
+
+variable "client_secret" {
+   default     = ""
+}
+
+variable "tenant_id" {
+   default     = "5fbeac1c-beae-48d4-8f09-540d25732ee1"
+}
+
+variable "resource_group_name" {
+   default     = "terraform_resource_group"
+}
+
+variable "resource_group_location" {
+   default     = "East us"
+}
+variable "virtual_network_name" {
+   default     = "terraform_vnet"
+}
+
+variable "subnet_name" {
+   default     = "terraform_subnet"
+}
+
+variable "storageaccountname" {
+    default     = "terrafromsa9536412"
+}
+
+variable "publicipaddress" {
+    default     = "terraformpubip"
+}
+
+variable "network_security_group_name" {
+    default     = "terraformnsg"
+}
+
+variable "network_interface_name" {
+    default     = "terraformnsg"
+}
+
+variable "windows_virtual_machine_name" {
+    default     = "terraformvm"
+}
